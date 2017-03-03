f484e8d88d7a4c30f573930df1aa229b61bf23d7
Chirag Chaplot <chiragchaplot@gmail.com>
2017-03-03 13:28:42 +0300

    Started the application
    Starting this application to create prime numbers

---
+++ Finding Prime Numbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Finding Prime Numbers
+//
+//  Created by Chirag Chaplot on 3/3/17.
+//  Copyright © 2017 ChazIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
