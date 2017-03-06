e34fd0210f944eec7d280f85692109423fa611a8
Chirag Chaplot <chiragchaplot@gmail.com>
2017-03-06 08:14:43 +0300

    Working with UI elements
    Worked out methods to maintain a constant UI amongst different screen
    elements.

---
+++ Prime-Number.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+var number = 47
+
+var isPrime = true
+
+for var i = 2; i < number; i++
+{
+    if number % i == 0
+    {
+        isPrime = false
+    }
+    else
+    {
+        continue;
+    }
+}
+
+print (isPrime)
+
+
+
