525576c2eada7a204379863b6923e87ba8d2135e
Chirag Chaplot <chiragchaplot@gmail.com>
2017-03-06 19:32:17 +0300

    learning more about layouts properly
    Layouts

--- Finding Prime Numbers/ViewController.swift
+++ Finding Prime Numbers/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var numTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func isItPrime(sender: AnyObject)
+    {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
