f4da146721a76782b65f5a7c4a623cc2da3ad5f4
Chirag Chaplot <chiragchaplot@gmail.com>
2017-03-12 22:35:07 +0300

    Finished the app
    Finished the app, added the labels , arranged all UI elements.

--- Finding Prime Numbers/ViewController.swift
+++ Finding Prime Numbers/ViewController.swift
@@ -14,8 +14,36 @@ class ViewController: UIViewController {
     
     @IBOutlet var resultLabel: UILabel!
     
+    
     @IBAction func isItPrime(sender: AnyObject)
     {
+        let number = Int(numTextField.text!)!
+        var isPrime = true
+        if number == 1
+        {
+            isPrime = false
+        }
+        if number != 2 && number != 1
+        {
+            for var i=2; i < number; i++
+            {
+                if number % i == 0
+                {
+                    isPrime = false
+                    break
+                }
+            }
+        }
+        
+        if isPrime
+        {
+            resultLabel.text = "isPrime"
+        }
+        else
+        {
+            resultLabel.text = "is not Prime"
+        }
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
